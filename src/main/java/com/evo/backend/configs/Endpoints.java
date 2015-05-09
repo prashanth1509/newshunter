432daacc59e96cbe479bfb2dd0e5eda7b78b2f16
prashanth.a <prashanth.a@flipkart.com>
2015-05-10 05:14:52 +0530

    update-endpoints

--- src/main/java/com/evo/backend/configs/Endpoints.java
+++ src/main/java/com/evo/backend/configs/Endpoints.java
@@ -11,14 +11,13 @@ public class Endpoints {
     public static final String ALL_HASHES = "https://api.myjson.com/bins/1uet9";
     public static final String CLASSIFIER = /*"http://localhost:8080/php-apps/test.php";*/ "http://127.0.0.1:8000";
 
-
     public static String getTwitterTrendsUrl(){
-        return Utils.isLocal()?"http://localhost:8087/twitter-api/trending":"";
+        return Utils.isLocal()?"http://localhost:8087/twitter-api/trending":"https://sleepy-cliffs-3321.herokuapp.com/twitter-api/trending";
         //return "https://api.myjson.com/bins/1njwd";
     }
 
     public static String getTwitterTagDataUrl(){
-        return Utils.isLocal()?"http://localhost:8087/twitter-api/getTexts":"";
+        return Utils.isLocal()?"http://localhost:8087/twitter-api/getTexts":"https://sleepy-cliffs-3321.herokuapp.com/twitter-api/getTexts";
     }
 
 
